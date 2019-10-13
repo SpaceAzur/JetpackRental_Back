465113707e7fb05b14c02b7a6b436095a472859c
François SAINTIER <f.saintier@gmail.com>
2019-10-13 11:02:57 +0200

    Implementation fonctionnalite getJetpackById + tests

--- controller/Jetpack/GetJetpackController.js
+++ controller/Jetpack/GetJetpackController.js
@@ -1,4 +1,21 @@
+const db = require('../../src/Db');
+const JetpackRepository = require('../../src/Repository/JetpackRepository');
+
 module.exports = (req, res) => {
-    var id = req.params.id;
-    res.send('Not implemented');
+  var my_id = req.params.id;
+
+  //console.log('id ' + my_id);
+  if (my_id === undefined){
+    console.log('Recuperation de la liste des jetpacks')
+    const repository = new JetpackRepository(db);
+    let jetpacks = repository.getAll();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send(jetpacks)
+  } else {
+    console.log('Recuperation jetpack id: ' + my_id);
+    const repository = new JetpackRepository(db);
+    let jetpack = repository.getJetpackById(my_id);
+    res.status(200).send(jetpack)
+  }
+
 };
--- src/Repository/JetpackRepository.js
+++ src/Repository/JetpackRepository.js
@@ -1,20 +1,36 @@
 module.exports = class {
-    constructor(db) {
-        this.db = db;
+  constructor(db) {
+      this.db = db;
+  }
+
+  create(jetpack) {
+      if (!jetpack) {
+          throw 'Jetpack object is undefined';
+      }
+
+      if (!jetpack.id || !jetpack.name) {
+          throw 'Jetpack object is missing information';
+      }
+
+      this.db
+          .get('jetpacks')
+          .push(jetpack.toJson())
+          .write()
+  }
+
+  getAll() {
+    return this.db.get('jetpacks').value();
+  }
+
+  getJetpackById(idToSearch){
+    if(idToSearch) {
+      return this.db
+        .get('jetpacks')
+        .find(({id: idToSearch}))
+        .value();
+    } else {
+      throw ('Unable to search empty jetpack ID');
     }
+  }
 
-    create(jetpack) {
-        if (!jetpack) {
-            throw 'Jetpack object is undefined';
-        }
-
-        if (!jetpack.id || !jetpack.name) {
-            throw 'Jetpack object is missing information';
-        }
-
-        this.db
-            .get('jetpacks')
-            .push(jetpack.toJson())
-            .write()
-    }
 };
