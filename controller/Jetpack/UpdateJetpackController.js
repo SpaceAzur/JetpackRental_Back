87bedb26b72639b57a229a4da2d32df3c1dda4ac
François SAINTIER <f.saintier@gmail.com>
2019-11-11 16:11:14 +0100

    Implementation update jetpacks

--- controller/Jetpack/UpdateJetpackController.js
+++ controller/Jetpack/UpdateJetpackController.js
@@ -1,12 +1,22 @@
+const Jetpack = require("../../src/Entity/Jetpack");
 const db = require('../../src/Db');
 const JetpackRepository = require('../../src/Repository/JetpackRepository.js');
 
 module.exports = (req, res) => {
-  let id_jetpack = req.params.id;
-  console.log("Suppression booking: " + id_jetpack);
+  let jetpack = new Jetpack();
+  jetpack.id = req.params.id;
+  jetpack.image = req.body.image;
+  jetpack.name = req.body.name;
+
+  console.log("Mise a jour jetpack: " + jetpack.id);
 
   const repository = new JetpackRepository(db);
-  repository.updateJetpack(id_jetpack);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.status(501).send();
+  try {
+    repository.updateJetpack(jetpack);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send(jetpack.toJson());
+  } catch (e) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(424).send(e);
+  }
 };
